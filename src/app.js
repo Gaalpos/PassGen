cc16b31247b67161e6b66bcaf3917d22708b818d
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-11-16 00:09:35 +0100

    Intentando deployar backend

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import './styles.scss'
 document.getElementById('btn').addEventListener('click', () => {
     let reqLength = document.getElementById('length')
 
-    fetch("http://localhost:4000/password/" + reqLength.value).then(res => res.json()).then(res => {
+    fetch("https://passgen-abelgarcia2.netlify.app/:4000" + reqLength.value).then(res => res.json()).then(res => {
         document.getElementById('pass').innerHTML = res.pass
     })
 
