0d14306b1587fe01c5b6c2ca232c133a4442717a
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-11-16 14:35:57 +0100

    Por fin funciona

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ document.getElementById('btn').addEventListener('click', () => {
     let reqLength = document.getElementById('length')
 
     fetch("https://pas-gen.herokuapp.com/password/" + reqLength.value).then(res => res.json()).then(res => {
-        document.getElementById('pass').innerHTML = res.pass
+        document.getElementById('pass').innerHTML = res[0].pass
     })
 
 })
