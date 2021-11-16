63ca342445ac2e63f57b20ecbeb6baddb3f36b8a
abelgarcia2 <65343232+abelgarcia2@users.noreply.github.com>
2021-11-16 14:00:53 +0100

    A ver si ahora

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import './styles.scss'
 document.getElementById('btn').addEventListener('click', () => {
     let reqLength = document.getElementById('length')
 
-    fetch("https://passgen-abelgarcia2.netlify.app/:4000" + reqLength.value).then(res => res.json()).then(res => {
+    fetch("https://pas-gen.herokuapp.com/password/" + reqLength.value).then(res => res.json()).then(res => {
         document.getElementById('pass').innerHTML = res.pass
     })
 
